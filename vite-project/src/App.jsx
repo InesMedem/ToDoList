9912657bbe2524a9417aae9b5852b6a80b3326cc
InesMedem <inesmedem93@gmail.com>
2024-03-27 18:34:04 +0100

    delete function

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -21,6 +21,23 @@ export default function App() {
     setNewItem("");
   };
 
+  const toggleTodo = (id, completed) => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  };
+
+  const deleteTodos = (id) => {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -37,14 +54,25 @@ export default function App() {
       </form>
       <h1 className="header">Todo list</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">delete</button>
+              <button
+                onClick={() => deleteTodos(todo.id)}
+                // have a function you call (via de callback) when the button is pressed
+                className="btn btn-danger"
+              >
+                delete
+              </button>
             </li>
           );
         })}
